d5636cf73eb82f60f5d9e1bd4ce245b2ebacc67b
Rodrigo Passos <rodrigopassosnascimento@gmail.com>
2024-03-22 14:06:50 -0300

    inicio e primeira etapa do projeto

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import { Game } from '../../pages/Home'
+import { Imagem, Precos, Titulo } from './styles'
+import { formataPreco } from '../ProductsList'
+
+import bannerImg from '../../assets/images/banner-homem-aranha.png'
+import Tag from '../Tag'
+import Button from '../Button'
+
+const Banner = () => {
+  const [game, setGame] = useState<Game>()
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/destaque')
+      .then((res) => res.json())
+      .then((res) => setGame(res))
+  }, [])
+
+  if (!game) {
+    return <h3>Carregando...</h3>
+  }
+
+  return (
+    <Imagem style={{ backgroundImage: `url(${game.media.cover})` }}>
+      <div className="container">
+        <Tag size="big">Destaque do dia</Tag>
+        <div>
+          <Titulo>{game.name}</Titulo>
+          <Precos>
+            De <span>{formataPreco(game.prices.old)}</span> <br />
+            por apenas {formataPreco(game.prices.current)}
+          </Precos>
+        </div>
+        <Button
+          type="link"
+          to={`/product/${game.id}`}
+          title="Clique aqui para aproveitar esta oferta"
+        >
+          Aproveitar
+        </Button>
+      </div>
+    </Imagem>
+  )
+}
+
+export default Banner
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,40 @@
+import { Banner, Infos } from './styles'
+import { Game } from '../../pages/Home'
+import { formataPreco } from '../ProductsList'
+import Tag from '../Tag'
+import Button from '../Button'
+
+type Props = {
+  game: Game
+}
+
+const Hero = ({ game }: Props) => (
+  <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
+    <div className="container">
+      <div>
+        <Tag>{game.details.category}</Tag>
+        <Tag>{game.details.system}</Tag>
+      </div>
+      <Infos>
+        <h2>{game.name}</h2>
+        <p>
+          {game.prices.discount && (
+            <span>De {formataPreco(game.prices.old)}</span>
+          )}
+          {game.prices.current && <>Por {formataPreco(game.prices.current)}</>}
+        </p>
+        {game.prices.current && (
+          <Button
+            type="button"
+            title="Clique aqui para adicionar  este jogo ao carrinho"
+            variant="primary"
+          >
+            Adicionar ao carrinho
+          </Button>
+        )}
+      </Infos>
+    </div>
+  </Banner>
+)
+
+export default Hero
